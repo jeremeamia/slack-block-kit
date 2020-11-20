2862aed3b3f95e80a3c51344e47f7c2b8ff2ecfd
Jeremy Lindblom <jeremeamia@gmail.com>
2020-11-20 11:50:29 -0700

    Added support for Message Attachments.
    Also changed KitRenderer to use the updated block kit builder URL.

--- tests/Surfaces/MessageTest.php
+++ tests/Surfaces/MessageTest.php
@@ -4,36 +4,46 @@ declare(strict_types=1);
 
 namespace Jeremeamia\Slack\BlockKit\Tests\Surfaces;
 
+use Jeremeamia\Slack\BlockKit\Exception;
 use Jeremeamia\Slack\BlockKit\Surfaces\Message;
 use Jeremeamia\Slack\BlockKit\Tests\TestCase;
+use Jeremeamia\Slack\BlockKit\Type;
 
 /**
  * @covers \Jeremeamia\Slack\BlockKit\Surfaces\Message
  */
 class MessageTest extends TestCase
 {
-    /**
-     * @param Message $message
-     * @param string $directiveKey
-     * @param string $directiveValue
-     * @dataProvider provideDirectivesUseCases
-     */
-    public function testCanApplyDirectives(Message $message, string $directiveKey, string $directiveValue)
+    public function testCanApplyEphemeralDirectives()
     {
-        $data = $message->text('foo')->toArray();
+        $data = Message::new()->ephemeral()->text('foo')->toArray();
         $this->assertArrayHasKey('blocks', $data);
-        $this->assertArrayHasKey($directiveKey, $data);
-        $this->assertEquals($directiveValue, $data[$directiveKey]);
+        $this->assertArrayHasKey('response_type', $data);
+        $this->assertEquals('ephemeral', $data['response_type']);
     }
 
-    public function provideDirectivesUseCases()
+    public function testCanApplyInChannelDirectives()
     {
-        return [
-            [Message::new()->ephemeral(), 'response_type', 'ephemeral'],
-            [Message::new()->inChannel(), 'response_type', 'in_channel'],
-            [Message::new()->replaceOriginal(), 'replace_original', 'true'],
-            [Message::new()->deleteOriginal(), 'delete_original', 'true'],
-        ];
+        $data = Message::new()->inChannel()->text('foo')->toArray();
+        $this->assertArrayHasKey('blocks', $data);
+        $this->assertArrayHasKey('response_type', $data);
+        $this->assertEquals('in_channel', $data['response_type']);
+    }
+
+    public function testCanApplyReplaceOriginalDirectives()
+    {
+        $data = Message::new()->replaceOriginal()->text('foo')->toArray();
+        $this->assertArrayHasKey('blocks', $data);
+        $this->assertArrayHasKey('replace_original', $data);
+        $this->assertEquals('true', $data['replace_original']);
+    }
+
+    public function testCanApplyDeleteOriginalDirectives()
+    {
+        $data = Message::new()->deleteOriginal()->text('foo')->toArray();
+        $this->assertArrayHasKey('blocks', $data);
+        $this->assertArrayHasKey('delete_original', $data);
+        $this->assertEquals('true', $data['delete_original']);
     }
 
     public function testDoesNotApplyDirectivesWhenNotSet()
@@ -43,5 +53,79 @@ class MessageTest extends TestCase
         $this->assertArrayNotHasKey('response_type', $data);
         $this->assertArrayNotHasKey('replace_original', $data);
         $this->assertArrayNotHasKey('delete_original', $data);
+    }
+
+    public function testCanAddAttachments()
+    {
+        $msg = Message::new()->tap(function (Message $msg) {
+            $msg->text('foo');
+            $msg->newAttachment()->text('bar');
+            $msg->newAttachment()->text('baz');
+        });
+
+        $this->assertJsonData([
+            'blocks' => [
+                [
+                    'type' => Type::SECTION,
+                    'text' => [
+                        'type' => Type::MRKDWNTEXT,
+                        'text' => 'foo',
+                    ],
+                ],
+            ],
+            'attachments' => [
+                [
+                    'blocks' => [
+                        [
+                            'type' => Type::SECTION,
+                            'text' => [
+                                'type' => Type::MRKDWNTEXT,
+                                'text' => 'bar',
+                            ],
+                        ],
+                    ],
+                ],
+                [
+                    'blocks' => [
+                        [
+                            'type' => Type::SECTION,
+                            'text' => [
+                                'type' => Type::MRKDWNTEXT,
+                                'text' => 'baz',
+                            ],
+                        ],
+                    ],
+                ],
+            ]
+        ], $msg);
+    }
+
+    public function testCanAddAttachmentsWithoutPrimaryBlocks()
+    {
+        $msg = Message::new()->tap(function (Message $msg) {
+            $msg->newAttachment()->text('foo');
+        });
+
+        $this->assertJsonData([
+            'attachments' => [
+                [
+                    'blocks' => [
+                        [
+                            'type' => Type::SECTION,
+                            'text' => [
+                                'type' => Type::MRKDWNTEXT,
+                                'text' => 'foo',
+                            ],
+                        ],
+                    ],
+                ],
+            ]
+        ], $msg);
+    }
+
+    public function testMustAddBlocksAndOrAttachments()
+    {
+        $this->expectException(Exception::class);
+        Message::new()->validate();
     }
 }
